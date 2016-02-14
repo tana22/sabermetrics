21711aedfcf9c60fef70ebce1c7823f29e055ab6
ryanplestid <ryan.plesti@gmail.com>
2016-02-14 12:20:36 -0500

    added documentation to my functions and increased the efficency in building the table

--- ryan_soup.py
+++ ryan_soup.py
@@ -8,12 +8,32 @@ from bs4 import BeautifulSoup
 error = ''
 
 
+def main():
+    url = "http://www.nhl.com/scores/htmlreports/20152016/TV020809.HTM"
+    array=getTextArray(url)
+
+    players=buildPlayerList(array)
+    events=getEventPos(array)
+   
+    table=buildShiftTable(array)
+
+    for n in range(0,len(players)):
+        print players[n], table[n]
+
+    #print array
+
+
 
 def getTextArray(url):
-    """Take in a url as an argument and applies bs4 to it
-    then it takes the soup and uses get_text. This is then
-    parsed by '\n' into a list and then blank entries are 
-    removed"""
+    '''
+    Take in a url as an argument and applies bs4 to it
+    then it takes the soup and uses get_text. The string 
+    returned by get_text is in unicode, this is then encoded
+    to ASCII with utf8. This ASCII string is then parsed 
+    by newlines into a list and then blank entries are removed
+    the function returns the resultant array of strings in 
+    utf-8 encoded ASCII
+    '''
     
     err ='errorRy.txt'
     r = requests.get(url)
@@ -34,9 +54,78 @@ def getTextArray(url):
     return newArray
 
 
+def buildShiftTable(array):
+    '''
+    This function takes the ASCII-encoded array
+    from getTextArray and returns a list of lists
+    where the nested lists contain all of the events
+    for a given player. They are organized in the
+    same order as buildPlayerList and so to see a 
+    players shifts on ice one could do
+    print playerName[__X__],shiftTable[__X__]
+    '''
+
+
+    table=[]
+    playerPositions=getPlayerPos(array)
+    eventPositions=getEventPos(array)
+
+
+    # This is probably the most inefficient way to 
+    # do this. This might be a bottleneck
+
+    # Cycle through each player
+    for playerPos in playerPositions:
+        setOfEvents=[]
+        start=0
+
+        #search through eventPos starting at 
+        # start and check when the current 
+        #player is found
+       
+        for eventPos in eventPositions[start:]:
+            if eventPos==playerPos:
+
+                #upon finding the player find what 
+                # index we are at
+                nEvent=eventPositions.index(eventPos)
+                nPlay=playerPositions.index(playerPos)
+
+                # if we are not at the end of the 
+                # list of players find where the 
+                # next player is 
+                if nPlay+1<len(playerPositions):
+                    nextPlayPos=playerPositions[nPlay+1]
+
+                # append all events to this current player's
+                # list of events until either a string with
+                # more than 3 chars is encountered (end of list)
+                # or the next player is encountered 
+                for  eventPos2 in eventPositions[nEvent+1:]:
+                    if eventPos2==nextPlayPos or len(array[eventPos2])>3:
+                        # re-set start so that the program does not 
+                        # loop through events it already recorded in
+                        # the first nested loop
+                        start=eventPositions.index(eventPos2)
+                        break
+                    else:
+                        # use position to gather events and append
+                        # them to the list of events for a player
+                        setOfEvents.append(buildEvent(array,eventPos2))
+
+        # add all the events for a player to their position in the table
+        table.append(setOfEvents)
+
+    return table
+
+
+
 def findAwayPos(array):
-    ''' Finds the position in an array of the string VISITOR
-    prints a warning and returns -1 if no such string is found'''
+    ''' 
+    Finds the position in an array of the string VISITOR
+    prints a warning and returns -1 if no such string is found
+    '''
+    
     for i in range (0,len(array)):
         if array[i]=='VISITOR':
             return i
@@ -44,8 +133,11 @@ def findAwayPos(array):
     return -1
 
 def findHomePos(array):
-    ''' Finds the position in an array of the string HOME
-    prints a warning and returns -1 if no such string is found'''
+    ''' 
+    Finds the position in an array of the string HOME
+    prints a warning and returns -1 if no such string is found
+    '''
+    
     for i in range (0,len(array)):
         if array[i]=='HOME':
             return i
@@ -53,7 +145,11 @@ def findHomePos(array):
     return -1
 
 def findHomeTeam(array):
-    '''returns string of home team'''
+    '''
+    takes ASCII encoded text array
+    returns ASCII encoded string of home team
+    '''
+
     spot=findHomePos(array)
     if spot==-1:
         return ''
@@ -62,7 +158,10 @@ def findHomeTeam(array):
         return raw.split('Game ')[0]
 
 def findAwayTeam(array):
-    '''returns string of home team'''
+    '''
+    takes ASCII encoded text array
+    returns ASCII encoded string of home team
+    '''
     spot=findAwayPos(array)
     if spot==-1:
         return ''
@@ -71,6 +170,11 @@ def findAwayTeam(array):
         return raw.split('Game ')[0]
 
 def findDateStr(array):
+    '''
+    takes ASCII encoded text array
+    Function to find the date of the game
+    '''
+
     spot=findAwayPos(array)
     if spot==-1:
         return ''
@@ -79,6 +183,11 @@ def findDateStr(array):
         return raw
 
 def getPlayerPos(array):
+    '''
+    returns a list of integers that are the 
+    index locatoions for players' names
+    in the text array passed to the function
+    '''
     playerPositions=[]
     for n in range(0,len(array)):
         item=array[n]
@@ -87,14 +196,25 @@ def getPlayerPos(array):
     return playerPositions
 
 def getEventPos(array):
+    '''
+    returns a list of integers with positions of any
+    'Event' in practice this is the date, a players 
+    name, an event ID, or the copyright at the bottom
+    '''
+
     eventPos=[]
     for n in range(0,len(array)):
         item=array[n]
-        #This encode call takes care of the 
-        #fact that bs4 ouputs unicode 
-        #google Unicode-How To FMI
-        if item =='\xc2\xa0' or item=='GP' or item=='EventG=GoalP=Penalty':
+        
+        # check for events that we like
+        if array[n] =='\xc2\xa0' or array[n] =='GP' or \\
+           array[n] =='EventG=GoalP=Penalty':
+            #filter out ones that we dont
             if array[n+1]!='\xc2\xa0' and array[n+1]!='Per':
+                # check if we are at the start of 
+                # this is just to deal with a weird 
+                # formatting irregularity 
+
                 if n+2==getPlayerPos(array)[0]:
                     eventPos.append(n+2)
                 else:
@@ -103,6 +223,10 @@ def getEventPos(array):
     return eventPos
 
 def buildPlayerList(array):
+    '''
+    returns a list of strings which are all of
+    the players full names and numbers
+    '''
     players=[]
     playerPos=getPlayerPos(array)
     
@@ -112,6 +236,11 @@ def buildPlayerList(array):
     return players
 
 def buildEvent(array,eventPos):
+    '''
+    This funciton takes an array and the position
+    of an event tag and returns a 3 entry list of 
+    strings with the period time on and time off
+    '''
     period=array[eventPos+1]
     time1=array[eventPos+2]
     time2=array[eventPos+3]
@@ -119,42 +248,8 @@ def buildEvent(array,eventPos):
     return [period,time1,time2]
 
 
-def buildEventTable(array):
-    table=[]
-    playerPositions=getPlayerPos(array)
-    eventPositions=getEventPos(array)
-
-    for playerPos in playerPositions:
-        setOfEvents=[]
-        for eventPos in eventPositions:
-            if eventPos==playerPos:
-                nEvent=eventPositions.index(eventPos)
-                nPlay=playerPositions.index(playerPos)
-                if nPlay+1<len(playerPositions):
-                    nextPlayPos=playerPositions[nPlay+1]
-                for  eventPos2 in eventPositions[nEvent+1:]:
-                    if eventPos2==nextPlayPos or len(array[eventPos2])>3:
-                        break
-                    else:
-                        setOfEvents.append(buildEvent(array,eventPos2))
-        table.append(setOfEvents)
-    return table
 
 
-
-
-def main():
-    url = "http://www.nhl.com/scores/htmlreports/20152016/TV020809.HTM"
-    array=getTextArray(url)
-
-    players=buildPlayerList(array)
-    events=getEventPos(array)
-   
-    table=buildEventTable(array)
-
-    print players[1], table[1]
-
-    #print array
 
 if __name__ == "__main__":
     main()
